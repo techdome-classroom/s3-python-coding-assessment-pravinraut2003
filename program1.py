650bcf0b5ff15150c89f215e78988c8e547357a9
My coding life <umeshachare2004.in>
2024-10-08 15:55:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -16,7 +16,7 @@ class Solution(object):
             else:
                 stack.append(char)
                 return not stack 
-            print(isvalid)
+            print(isvalid("()"))
 
         pass
 
